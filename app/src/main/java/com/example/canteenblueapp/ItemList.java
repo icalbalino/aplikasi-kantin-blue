1f1995e7bf42cfa19bbe8b7d14775a71ea3e797b
icalbalino <balino.ical@gmail.com>
2022-04-16 20:58:50 +0700

    add assets

--- app/src/main/java/com/example/canteenblueapp/ItemList.java
+++ app/src/main/java/com/example/canteenblueapp/ItemList.java
@@ -4,22 +4,40 @@ import java.util.ArrayList;
 
 public class ItemList {
     private static String[] nameItem = {
-            "Dim sam",
-            "Dim 1",
-            "dim"
+            "Shrimp Dumplings",
+            "Soup Dumplings",
+            "Stuffed Eggplant",
+            "Build your own",
+            "MeatLovers",
+            "Veggie",
+            "Taco",
+            "Quesadilla",
+            "Burrito"
     };
 
     private static float[] price = {
-            2,
-            3,
-            5
+            (float) 6.49,
+            (float) 9.99,
+            (float) 5.59,
+            (float) 17.99,
+            (float) 18.49,
+            (float) 17.49,
+            (float) 3.45,
+            (float) 6.49,
+            (float) 5.59
     };
+
     private  static  int[] pic={
             R.mipmap.dimsum_eggplant,
             R.mipmap.dimsum_shrimp,
-            R.mipmap.dimsum_soup
+            R.mipmap.dimsum_soup,
+            R.mipmap.pizza_plain,
+            R.mipmap.pizza_meat,
+            R.mipmap.pizza_veggie,
+            R.mipmap.taco_taco,
+            R.mipmap.taco_quesadilla,
+            R.mipmap.taco_burrito
     };
-
 
     static ArrayList<item> getListData() {
         ArrayList<item> list = new ArrayList<>();
